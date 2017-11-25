3e01e0cc66ea23d383e7f8bec8e22608c9f3ee85
sxp <sxp>
2017-11-25 20:37:35 +0800

    grooy shell Test

---
+++ src/main/java/com/sxp/script/DataFilter.java
@@ -0,0 +1,104 @@
+package com.sxp.script;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import java.util.*;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author sxp
+ * @create 2017/11/25.
+ */
+public class DataFilter {
+
+    public static void main(String[] args) {
+        //String alaisConfig="{'real':'alias'}";
+        Map<String, Object> row = new HashMap<String, Object>();
+        row.put("real", 3);
+        row.put("normal", "normal");
+        row.put("ignore", "ignore");
+        row.put("operate", "5");
+
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        list.add(row);
+
+        List<Map<String, Object>> filter = new DataFilter().filter(list);
+
+        System.out.println(JSON.toJSONString(filter));
+
+       // Integer.valueOf()
+    }
+
+    static String[] ignores = new String[]{"ignore"};
+    static List<String> ignoreList = Arrays.asList(ignores);
+
+
+    static String alaisConfig = "{'real':'alias'}";
+    JSONObject alaisMap = JSON.parseObject(alaisConfig);
+
+    static String addConfig = "{'add':'real+2'}";
+    JSONObject addMap = JSON.parseObject(addConfig);
+
+    static String operateConfig = "{'operate':'(Integer.valueOf(real+operate))*100'}";
+    static JSONObject operateMap = JSON.parseObject(operateConfig);
+    /**
+     * 对 List<Map> 进行过滤
+     *
+     * @param dataList
+     * @return
+     */
+    private List<Map<String, Object>> filter(List<Map<String, Object>> dataList) {
+        //ignore field /alias field/add field/change field value
+        List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+        for (Map<String, Object> oldRow : dataList) {
+
+            //对每行进行操作
+            //
+            Map<String, Object> newRow = new HashMap<String, Object>();
+            for (Map.Entry<String, Object> entry : oldRow.entrySet()) {
+                //ignore
+                if (ignoreList.contains(entry.getKey()))
+                    continue;
+
+                //alais
+                if (alaisMap.containsKey(entry.getKey()))
+                    newRow.put((String) alaisMap.get(entry.getKey()), entry.getValue());
+                //operate
+                else if (operateMap.containsKey(entry.getKey()))
+                    newRow.put(entry.getKey(), getOperateValue(oldRow, entry.getKey(), operateMap));
+                else
+                    newRow.put(entry.getKey(), entry.getValue());
+
+            }
+
+            //add
+            for (Map.Entry<String, Object> addItem : addMap.entrySet()) {
+                newRow.put(addItem.getKey(), getOperateValue(oldRow, addItem.getKey(), addMap));
+            }
+
+            if (newRow.size() > 0)
+                resultList.add(newRow);
+
+        }
+
+        return resultList;
+    }
+
+    private static String getOperateValue(Map<String, Object> bindingMap, String key, Map opMap) {
+        Binding binding = new Binding();
+
+        for (Map.Entry<String, Object> entry : bindingMap.entrySet()) {
+            binding.setVariable(entry.getKey(), entry.getValue());
+        }
+
+        GroovyShell gs = new GroovyShell(binding);
+        Object evaluate = gs.evaluate("return " + opMap.get(key));
+
+        return evaluate.toString();
+    }
+
+}
